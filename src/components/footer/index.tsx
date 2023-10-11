5c49cbbb53c5731da4f19ff1f87e57ccbc45850f
anovic123 <anovicjosip@gmail.com>
2023-10-11 15:00:20 +0300

    added scroll to sections for footer links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,6 +3,10 @@
 import Link from 'next/link'
 import { ReactElement } from 'react'
 
+import { casesRef } from '@/sections/sales-info'
+import { featuresRef } from '@/sections/features-block'
+import { startRef } from '@/sections/instruction-block'
+
 import { useMediaQuery } from '@/hooks/use-media-query'
 import LOGO from 'public/img/header/logo.svg'
 import LOGO_MOBILE from 'public/img/header/logo-mobile.svg'
@@ -11,8 +15,23 @@ import { Button } from '../ui/button'
 
 import s from './footer.module.scss'
 
+interface NavRef {
+    current: HTMLDivElement | null;
+}
+
 export function Footer (): ReactElement {
     const isMobile = useMediaQuery(768)
+
+    const scroll = (ref: NavRef) => {
+        if (ref.current) {
+            ref.current.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
+
+    const handleLinkClick = (event: React.MouseEvent, ref: NavRef) => {
+        event.preventDefault()
+        scroll(ref)
+    }
 
     return (
         <footer className={s.footer}>
@@ -24,17 +43,17 @@ export function Footer (): ReactElement {
                     <nav className={s.footerNav}>
                         <ul className={s.footerList}>
                             <li>
-                                <Link className={s.footerLink} href="/">
+                                <Link className={s.footerLink} href="#" onClick={event => handleLinkClick(event, casesRef)}>
                                     Use cases
                                 </Link>
                             </li>
                             <li>
-                                <Link className={s.footerLink} href="/">
+                                <Link className={s.footerLink} href="#" onClick={event => handleLinkClick(event, featuresRef)}>
                                     Features
                                 </Link>
                             </li>
                             <li>
-                                <Link className={s.footerLink} href="/">
+                                <Link className={s.footerLink} href="#" onClick={event => handleLinkClick(event, startRef)}>
                                     How to start
                                 </Link>
                             </li>
